fcdcf877af3c823b20cc3581468bf304922b8cb9
liujinxing <liujinxing@che.com>
2017-09-28 11:39:12 +0800

    liujx 添加厂商模块处理成功-以及优化功能完成

--- backend/views/partner/update.php
+++ backend/views/partner/update.php
@@ -1,6 +1,6 @@
 <?php
 
-use yii\helpers\Html;
+use yii\helpers\Json;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Partner */
@@ -13,13 +13,13 @@ $this->params['breadcrumbs'][] = '更新商户';
 <div class="box-body">
     <div class="nav-tabs-custom">
         <!-- Tabs within a box -->
-        <ul class="nav nav-tabs pull-left">
+        <ul class="nav nav-tabs pull-left" id="ul-div">
             <li class="active">
                 <a href="#basic-info" data-toggle="tab" aria-expanded="true">基本信息</a>
             </li>
-            <li class=""><a href="#dealer-info" data-toggle="tab" aria-expanded="false">经销商信息</a></li>
-            <li class=""><a href="#store-info-div" data-toggle="tab" aria-expanded="false">门店信息</a></li>
-            <li class=""><a href="#financial-info" data-toggle="tab" aria-expanded="false">金融管理</a></li>
+            <li class=""><a href="#dealer-info" styel="display:none" v-show="dealer" data-toggle="tab" aria-expanded="false">经销商信息</a></li>
+            <li class=""><a href="#store-info-div" styel="display:none" v-show="store" data-toggle="tab" aria-expanded="false">门店信息</a></li>
+            <li class=""><a href="#financial-info" styel="display:none" v-show="financial" data-toggle="tab" aria-expanded="false">金融管理</a></li>
         </ul>
         <div class="tab-content no-padding">
             <!-- 基本信息 -->
@@ -51,5 +51,45 @@ $this->params['breadcrumbs'][] = '更新商户';
 <?php $this->beginBlock('javascript') ?>
 <?=$this->blocks['javascript-dealer'] ?>
 <?=$this->blocks['javascript-store'] ?>
+<script>
+    var vueTab = new Vue({
+        el: "#ul-div",
+        data: {
+            item: <?=Json::encode($model->partner_identity)?>
+        },
+
+        methods: {
+            // 判断值是否在列表中
+            equal: function(value) {
+                for (var i in this.item) {
+                    if (parseInt(this.item[i]) === value) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        },
+
+        // 计算属性
+        computed: {
+            dealer: function() {
+                return this.equal(1);
+            },
+            store: function() {
+                return this.equal(2);
+            },
+            financial: function() {
+                return this.equal(3);
+            }
+        }
+
+    });
+
+    $("#partnerform-partner_identity").on("change", function(){
+        var arr = $(this).val();
+        vueTab.item = arr ? arr : [];
+    });
+</script>
 <?php $this->endBlock(); ?>
 
