04bc71f0334ad5c7143ad70d05974ade73af5910
liujinxing <liujinxing@che.com>
2017-09-29 17:02:55 +0800

    liujx 添加车型的基础配置信息

---
+++ frontend/controllers/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Class BaseController
+ *  基础的控制器,所有的api 控制器必须基础该基础控制器
+ *
+ * @package frontend\controllers
+ */
+class BaseController extends Controller
+{
+
+}
